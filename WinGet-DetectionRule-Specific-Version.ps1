7930b8804b1e759f240fdc8aff4252f75884d439
SorenLundt <127216441+SorenLundt@users.noreply.github.com>
2023-03-09 15:08:42 +0100

    Add files via upload

---
+++ WinGet-DetectionRule-Specific-Version.ps1
@@ -0,0 +1,61 @@
+﻿# Soren Lundt - 22-02-2023 - https://github.com/SorenLundt/WinGet-Wrapper
+# Version History
+# Version 1.1 - 27-02-2023 SOLU - Updated to retrieve current version from WinGet and dynamically check for latest version and detect based.
+# Version 1.2 - 28-02-2023 SOLU - Added logging capability
+# Version 1.3 - 01-03-2023 SOLU/ChatGPT - Changed expression to properly find the local installed version. Issue that winget list also shows available version from winget
+# Version 1.4 - 01-03-2023 SOLU - Added section to cleanup old log files. 60 days.
+
+#Define Package ID
+$id = "Exact Package ID"
+$TargetVersion = "1.0.0.0"
+
+#Create log folder
+if (!(Test-Path -Path $env:ProgramData\WinGet-WrapperLogs)) {
+    New-Item -Path $env:ProgramData\WinGet-WrapperLogs -Force -ItemType Directory
+}
+
+#TimeStamp
+$TimeStamp = "{0:MM-dd-yy}_{0:HH-mm-ss}" -f (Get-Date)
+
+#Start Logging
+Start-Transcript -Path "$env:ProgramData\WinGet-WrapperLogs\$($ID)_WinGet_Detection_$($TimeStamp).log"
+
+#Clean log files older than X days
+$Path = "$env:ProgramData\WinGet-WrapperLogs"
+$Days = 60
+$CutoffDate = (Get-Date).AddDays(-$Days)
+$FilesToDelete = Get-ChildItem $Path -Recurse -Include *.log | Where-Object LastWriteTime -lt $CutoffDate
+$Count = $FilesToDelete.Count
+$FilesToDelete | Remove-Item -Force
+Write-Host "Cleaned up a total of $Count old logs older than $Days days."
+
+#Find WinGet.exe Location
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+if ($ResolveWingetPath){
+    $WingetPath = $ResolveWingetPath[-1].Path
+}
+Set-Location $WingetPath
+
+#Get version installed locally on machine
+$searchstring = .\winget.exe list $id --exact --accept-source-agreements
+$versions = [regex]::Matches($searchstring, "$id\s+([\d\.]+)").Groups[1].Value
+if ($versions) {
+    $InstalledVersion = ($versions | sort {[version]$_} | select -Last 1)
+    Write-Host "Target Version:    $TargetVersion"
+    Write-Host "Installed version: $InstalledVersion"
+} else {
+    Write-Host "Package not found"
+}
+
+# Check if version on WinGet and local machine matches
+if ($TargetVersion -eq $InstalledVersion)
+{
+    Write-Output "Exit 0 - Report Installed"
+    exit 0 # exit 0 - report installed
+    
+}
+else
+{
+    Write-Output "Exit 1 - Report Not Installed"
+    exit 1 # exit 1 - report not installed
+}
---
+++ WinGet-Wrapper.ps1
@@ -0,0 +1,65 @@
+# Soren Lundt - 22-02-2023 - https://github.com/SorenLundt/WinGet-Wrapper
+# Wrapper for running winget in system context. Finds the WinGet install path and runs desired command
+#
+# Usage: .\WinGet-Wrapper.ps1 -PackageName "Packagename used in log entry" -StopProcess "kill process using Stop-Process (.exe for the most part is not needed)" -ArgumentList "Arguments Passed to WinGet.exe"
+# INSTALL   .\WinGet-Wrapper.ps1 -PackageName "VideoLAN.VLC" -StopProcess "VLC" -ArgumentList "install --exact --id VideoLAN.VLC --silent --accept-package-agreements --accept-source-agreements --scope machine"
+# UNINSTALL .\WinGet-Wrapper.ps1 -PackageName "VideoLAN.VLC" -StopProcess "VLC" -ArgumentList "Uninstall --exact --id VideoLAN.VLC --silent --accept-source-agreements --scope machine"
+#
+# Variables:
+# PackageName = Package name mainly used for naming the log file.
+# StopProcess = Kill a specific process (Stop-process) before executing WinGet command (.exe should not be defined) Skips any error automatically.
+# ArgumentList = Arguments passed directly to WinGet
+#
+# Version History
+# Version 1.0 - 22-02-2023 SOLU - Initial Version
+# Version 1.1 - 28-02-2023 SOLU - Added logging functionality and changed to use Invoke-Expression to get data written to prompt and log file
+# Version 1.2 - 01-03-2023 SOLU - Added logging of winget output + added PackageName parameter used in logging functionality
+# Version 1.3 - 02-03-2023 SOLU - Added stop process to kill any process before upgrading. No user warning is shown etc.
+
+Param (
+    [Parameter(Mandatory=$true)]
+    [string]$PackageName,
+
+    [Parameter()]
+    [string]$StopProcess,
+
+    [Parameter(Mandatory=$true)]
+    [string]$ArgumentList
+)
+
+  #Create log folder
+if (!(Test-Path -Path $env:ProgramData\WinGetLogs)) {
+    New-Item -Path $env:ProgramData\WinGetLogs -Force -ItemType Directory
+}
+
+#TimeStamp
+$TimeStamp = "{0:MM-dd-yy}_{0:HH-mm-ss}" -f (Get-Date)
+Write-Host "PackageName: $PackageName"
+Write-Host "ArgumentList: $Arguments"
+Write-Host "Timestamp: $TimeStamp"
+
+#Start Logging
+Start-Transcript -Path "$env:ProgramData\\WinGetLogs\$($PackageName)_WinGet_Wrapper_$($TimeStamp).log"
+
+#Stop process
+if (-not ($StopProcess -eq $null) -and $StopProcess -ne "") {
+    Stop-Process -Name $StopProcess -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+    Write-Host "Stopped process: $StopProcess"
+}
+
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+   if ($ResolveWingetPath){
+           $WingetPath = $ResolveWingetPath[-1].Path
+    }
+Set-Location $wingetPath
+$stdout = "$env:ProgramData\WinGet-WrapperLogs\StdOut-$timestamp.txt"
+$errout = "$env:ProgramData\WinGet-WrapperLogs\ErrOut-$timestamp.txt"
+
+Write-Host "Executing $wingetPath\Winget.exe $ArgumentList"
+Start-Process "$WingetPath\winget.exe" -ArgumentList "$ArgumentList" -PassThru -Wait -RedirectStandardOutput "$stdout" -RedirectStandardError "$errout"
+
+get-content "$stdout"
+get-content "$errout"
+Remove-item -Path "$stdout"
+Remove-item -Path "$errout"
+write-host "Script Finished"
