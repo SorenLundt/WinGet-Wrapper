02e19c923a8a6e345bad93bbb7518805f7c5f176
Søren Lundt <127216441+SorenLundt@users.noreply.github.com>
2023-08-21 13:18:19 +0200

    Add files via upload
    Winget-WrapperUpdateDetection.ps1  - Version 1.0
    # Use as detection script for required deployments to update applications if installed. 
    # If application is not installed initially, it will not install it.
    
    WinGet-WrapperDetection.ps1
    # Version 2.0 - 21-08-2023 SOLU - AutoUpdate Feature does not work well with Available deployment type in InTune, due to InTune not
    # Version 2.0 - 21-08-2023 SOLU - AutoUpdate Feature does not work well with Available deployment type in InTune, due to InTune not

--- WinGet-WrapperUpdateDetection.ps1
+++ WinGet-WrapperUpdateDetection.ps1
@@ -1,23 +1,18 @@
-﻿# Soren Lundt - 22-02-2023 - https://github.com/SorenLundt/WinGet-Wrapper
-# Version History
-# Version 1.1 - 27-02-2023 SOLU - Updated to retrieve current version from WinGet and dynamically check for latest version and detect based.
-# Version 1.2 - 28-02-2023 SOLU - Added logging capability
-# Version 1.3 - 01-03-2023 SOLU/ChatGPT - Changed expression to properly find the local installed version. Issue that winget list also shows available version from winget
-# Version 1.4 - 01-03-2023 SOLU - Added section to cleanup old log files. 60 days.
-# Version 1.5 - 13-03-2023 SOLU - Overall added better error handling - try/catch - Write-Output
-# Version 1.6 - 13-03-2023 SOLU - Added functionality to report installed if locally installed version is newer than Available WinGet or TargetVersion. $AcceptNewerVersion
-# Version 1.7 - 14-03-2023 SOLU - Added functionality to auto update - fixes issue #1 on https://github.com/SorenLundt/WinGet-Wrapper.
-# Version 1.8 - 26-05-2023 SOLU - Added support for context choice by adding $Context variable
-# Version 1.9 - 30-05-2023 SOLU - Fixed issues with running in user context (Winget path issues) + Updated version regex to be more precise
-# Version 2.0 - 21-08-2023 SOLU - AutoUpdate Feature does not work well with Available deployment type in InTune, due to InTune not
+﻿# Soren Lundt - 21-08-2023 - https://github.com/SorenLundt/WinGet-Wrapper
+#
+# Purpose:
+# Use as detection script for required deployments to update applications if installed. 
+# If application is not installed initially, it will not install it.
+#
+# Version History:
+# Version 1.0 - 21-08-2023 SOLU - Inital version "Winget-WrapperUpdateDetection" 
+#
+
 
 # Settings
-$id = "Exact WinGet package ID" # WinGet Package ID - ex. VideoLAN.VLC
+$id = "VideoLAN.VLC" # WinGet Package ID - ex. VideoLAN.VLC
 $TargetVersion = ""  # Set if specific version is desired (Optional)
 $AcceptNewerVersion = $True   # Allows locally installed versions to be newer than $TargetVersion or available WinGet package version
-$AutoUpdate = $False # If $True will update application if newer available on winget. Modify arguments below in $AutoUpdateArgumentList
-$AutoUpdateArgumentList = "update --exact --id $id --silent --disable-interactivity --accept-package-agreements --accept-source-agreements --scope Machine"
-$AutoUpdateStopProcess = "" # Stop-process if set, blank no process is stopped before update
 $Context = "System" # Set to either System or User
 
 # Create log folder
@@ -43,8 +38,13 @@ catch {
     exit 1
 }
 
-#Write the AcceptNewerVersion setting to log
+#Write useful variable settings to log
+Write-OutPut "ID = $id"
+Write-OutPut "TargetVersion = $TargetVersion  (Blank = Latest Version)"
 Write-OutPut "AcceptNewerVersion = $AcceptNewerVersion"
+Write-OutPut "Context = $Context"
+
+
 
 # Clean log files older than X days
 $daysToKeep = 60
@@ -52,7 +52,7 @@ $filesToDelete = Get-ChildItem $logPath -Recurse -Include *.log | Where-Object L
 try {
     $count = $filesToDelete.Count
     $filesToDelete | Remove-Item -Force | Out-Null
-    Write-Output "Cleaned up a total of $count old logs older than $daysToKeep days."
+    Write-Output "Cleaned up $count old logs older than $daysToKeep days."
 }
 catch {
     Write-Output "Failed to delete old log files: $($_.Exception.Message)"
@@ -60,14 +60,13 @@ catch {
 
 # Find WinGet.exe Location
 if ($Context -contains "System"){
-Write-Output "Running in System Context"
 try {
     $resolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
     if ($resolveWingetPath) {
         $wingetPath = $resolveWingetPath[-1].Path
         Set-Location $wingetPath
         $wingetPath = $wingetPath + "\winget.exe"
-        Write-Output "WinGet full path: $wingetPath"
+        Write-Output "WinGet path: $wingetPath"
     }
     else {
         Write-Output "Failed to find WinGet path"
@@ -103,6 +102,7 @@ catch {
 }elseif ($TargetVersion -ne $null -and $TargetVersion -ne '')
 { Write-Host "TargetVersion: $TargetVersion (Set specific)"}
 
+# Write out Application Name
 
 # Get version installed locally on machine
 $InstalledVersion = $null  # Clear Variable
@@ -126,40 +126,14 @@ $versions = [regex]::Matches($searchString, "(?m)^.*$id\s*(?:[>]?[\s]*)([\d.]+).
     }
 }
 catch {
-    Write-Output "Failed to get installed version: $($_.Exception.Message)"
-    exit 1
+    Write-Output "Application '$id' not installed. Nothing to update"
+    exit 0
 }
 
 
 if ($InstalledVersion -ge $TargetVersion -or ($AcceptNewerVersion -and $InstalledVersion -gt $TargetVersion)){
     Write-Output "Exit 0 - Report Installed"
     exit 0 # exit 0 - report installed 
-}
-elseif ($AutoUpdate -eq $True){
-
-   #Stop process
-    if (-not ($AutoUpdateStopProcess -eq $null) -and $AutoUpdateStopProcess -ne "") {
-    Stop-Process -Name $AutoUpdateStopProcess -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-    Write-Host "Stopped process: $AutoUpdateStopProcess"
-        }
-
-    $stdout = "$env:ProgramData\WinGet-WrapperLogs\StdOut-$timestamp.txt"
-    $errout = "$env:ProgramData\WinGet-WrapperLogs\ErrOut-$timestamp.txt"
-    
-    Write-Host "Executing Winget.exe $AutoUpdateArgumentList"
-    if ($Context -contains "System"){
-    Start-Process "$WingetPath\winget.exe" -ArgumentList "$AutoUpdateArgumentList" -PassThru -Wait -RedirectStandardOutput "$stdout" -RedirectStandardError "$errout"
-    }
-    else {
-    Start-Process "winget.exe" -ArgumentList "$AutoUpdateArgumentList" -PassThru -Wait -RedirectStandardOutput "$stdout" -RedirectStandardError "$errout"
-    }
-    
-    get-content "$stdout"
-    get-content "$errout"
-    Remove-item -Path "$stdout"
-    Remove-item -Path "$errout"
-    Write-Output "Exit 0 - Report Installed"
-    exit 0 # exit 0 - updated to latest version
 }
 else {
     Write-Output "Exit 1 - Report Not Installed"
